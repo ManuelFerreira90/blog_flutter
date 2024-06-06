b701179c02ee42b104ce571b66b1a93354bd5c7f
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-06 17:55:31 -0300

    feat: constantes para estilo da página

---
+++ lib/themes/style/consts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kTitleStyleAuthPages = TextStyle(
+  color: Colors.white,
+  fontSize: 40,
+  fontWeight: FontWeight.bold,
+);
+const kLabelStyleAuthPages = TextStyle(color: Colors.grey, fontSize: 14);
+const kPaddingAuthPages = EdgeInsets.symmetric(vertical: 10, horizontal: 25);
+const kBoderTextFormField =
+    OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)));
+const kFocusedBorderTextFormField = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(20)),
+    borderSide: BorderSide(color: Colors.white));
