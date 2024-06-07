c7150220fb553eaeddf691fb013d7e61dc94178a
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-07 15:11:35 -0300

    refactor: alterar retorno da função login

--- lib/api/auth/auth_service.dart
+++ lib/api/auth/auth_service.dart
@@ -7,7 +7,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  static void login(String userName, String password) async {
+  static Future<Map<String, dynamic>?> login(String userName, String password) async {
     try {
       var url = Uri.https(Endpoints.urlApi, Endpoints.authLogin);
       Map<String, dynamic> body = {
@@ -21,22 +21,37 @@ class AuthService {
       final response = await http.post(url,
           body: jsonBody, headers: {'Content-Type': 'application/json'});
 
-      if (response.statusCode == 200) {
-        Map<String, dynamic> user = jsonDecode(response.body);
+      Map<String, dynamic> responseBody = jsonDecode(response.body);
 
+      if (response.statusCode == 200) {
         SharedPreferences prefs = await SharedPreferences.getInstance();
-        prefs.setString('token', user['token']);
+        prefs.setString('token', responseBody['token']);
         
+        return {
+          'sucess': true,
+        };
       } else {
-        print('error: status code ${response.statusCode}');
+        return {
+          'sucess': false,
+          'error': responseBody['message'],
+        };
       }
     } catch (error) {
       if (error is HttpException) {
-        print('Network error: ${error.message}');
+        return {
+          'sucess': false,
+          'error': 'Network error: ${error.message}',
+        };
       } else if (error is FormatException) {
-        print('JSON parsing error: ${error.message}');
+        return {
+          'sucess': false,
+          'error': 'JSON parsing error: ${error.message}',
+        };
       } else {
-        print('Unexpected error: $error');
+        return {
+          'sucess': false,
+          'error': 'Unexpected error: $error',
+        };
       }
     }
   }
@@ -63,22 +78,19 @@ class AuthService {
               user['country'],
               user['phone']);
         } else {
-          print('error: status code ${response.statusCode}');
           return null;
         }
       } else {
-        print('sem token');
         return null;
       }
     } catch (error) {
       if (error is HttpException) {
-        print('Network error: ${error.message}');
+        return null;
       } else if (error is FormatException) {
-        print('JSON parsing error: ${error.message}');
+        return null;
       } else {
-        print('Unexpected error: $error');
+        return null;
       }
-      return null;
     }
   }
 }
