f26df283d0005a8f796671499bd8561fc1d4af81
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-08 13:09:52 -0300

    feat: cores para o app

---
+++ lib/themes/style/theme_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const colorScaffold = Color(0xff242529);
+  static const colorBottonSelected = Color(0xfffb032a);
+  static const colorDiviser = Color(0xff3b3d42);
+  static const colorInfoPost = Color(0xff515d67);
+}
