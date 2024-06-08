c4070ca533a419c4f12364bd333d81b2c8887ade
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-08 11:57:58 -0300

    refactor: passagem id nas funções check e register

--- lib/api/auth/auth_service.dart
+++ lib/api/auth/auth_service.dart
@@ -7,7 +7,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  static Future<Map<String, dynamic>?> login(String userName, String password) async {
+  static Future<Map<String, dynamic>> login(
+      String userName, String password) async {
     try {
       var url = Uri.https(Endpoints.urlApi, Endpoints.authLogin);
       Map<String, dynamic> body = {
@@ -26,7 +27,7 @@ class AuthService {
       if (response.statusCode == 200) {
         SharedPreferences prefs = await SharedPreferences.getInstance();
         prefs.setString('token', responseBody['token']);
-        
+
         return {
           'sucess': true,
         };
@@ -69,6 +70,7 @@ class AuthService {
           Map<String, dynamic> user = jsonDecode(response.body);
 
           return User(
+              user['id'],
               user['firstName'],
               user['lastName'],
               user['email'],
@@ -90,6 +92,47 @@ class AuthService {
         return null;
       } else {
         return null;
+      }
+    }
+  }
+
+  static Future<Map<String, dynamic>> register(
+      String userName, String phone, String email, String password) async {
+    try {
+      var url = Uri.https(Endpoints.urlApi, Endpoints.addUser);
+      Map<String, dynamic> body = {
+        'username': userName.trim(),
+        'phone': phone.trim(),
+        'email': email.trim(),
+        'password': password.trim(),
+      };
+
+      var jsonBody = jsonEncode(body);
+
+      final response = await http.post(url,
+          body: jsonBody, headers: {'Content-Type': 'application/json'});
+      var responseBody = jsonDecode(response.body);
+      if (response.statusCode == 201) {
+        return {'sucess': true, 'user': responseBody};
+      } else {
+        return {'sucess': false, 'error': '${responseBody['message']}'};
+      }
+    } catch (error) {
+      if (error is HttpException) {
+        return {
+          'sucess': false,
+          'error': 'Network error: ${error.message}',
+        };
+      } else if (error is FormatException) {
+        return {
+          'sucess': false,
+          'error': 'JSON parsing error: ${error.message}',
+        };
+      } else {
+        return {
+          'sucess': false,
+          'error': 'Unexpected error: $error',
+        };
       }
     }
   }
