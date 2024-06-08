85368d755a431b1e6dab39702334ace40675f364
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-08 11:54:58 -0300

    feat: validação email, telefone e função registerUser

--- lib/view/auth/register_page.dart
+++ lib/view/auth/register_page.dart
@@ -1,8 +1,15 @@
+import 'package:blog_mobile/controllers/auth/resgister_controller.dart';
+import 'package:blog_mobile/models/user.dart';
 import 'package:blog_mobile/themes/style/consts.dart';
 import 'package:blog_mobile/utils/auth/auth_utils.dart';
 import 'package:blog_mobile/view/auth/components/auth_button.dart';
-import 'package:blog_mobile/view/auth/login_page.dart';
+import 'package:blog_mobile/view/auth/components/auth_snackbar.dart';
+import 'package:blog_mobile/view/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:phone_numbers_parser/phone_numbers_parser.dart'
+    as phone_numbers_parser;
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -16,9 +23,12 @@ class _RegisterPageState extends State<RegisterPage> {
   late TextEditingController _phoneController;
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
+  late ResgisterController checkRegister;
   final _formKey = GlobalKey<FormState>();
   bool _isVisibly = false;
   bool _isAccepted = false;
+  PhoneNumber number = PhoneNumber(isoCode: 'BR');
+  String? userPhone = '';
 
   @override
   void initState() {
@@ -27,6 +37,7 @@ class _RegisterPageState extends State<RegisterPage> {
     _phoneController = TextEditingController();
     _emailController = TextEditingController();
     _passwordController = TextEditingController();
+    checkRegister = ResgisterController();
   }
 
   @override
@@ -90,6 +101,52 @@ class _RegisterPageState extends State<RegisterPage> {
                       const SizedBox(
                         height: 10,
                       ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: InternationalPhoneNumberInput(
+                          inputDecoration: const InputDecoration(
+                              labelText: 'Phone',
+                              labelStyle: kLabelStyleAuthPages,
+                              border: kBoderTextFormField,
+                              focusedBorder: kFocusedBorderTextFormField,
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.never),
+                          validator: (userInput) {
+                            final phoneCopy =
+                                phone_numbers_parser.PhoneNumber.parse(
+                                    userPhone ?? '');
+                            final valid = phoneCopy.isValid();
+                            if (!valid) {
+                              return 'Please enter your phone number';
+                            }
+                            return null;
+                          },
+                          onInputChanged: (PhoneNumber number) {
+                            userPhone = number.phoneNumber;
+                          },
+                          onInputValidated: (bool value) {},
+                          selectorConfig: const SelectorConfig(
+                            selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                            useBottomSheetSafeArea: true,
+                          ),
+                          ignoreBlank: false,
+                          autoValidateMode: AutovalidateMode.disabled,
+                          selectorTextStyle:
+                              const TextStyle(color: Colors.white),
+                          initialValue: number,
+                          textFieldController: _phoneController,
+                          formatInput: true,
+                          keyboardType: const TextInputType.numberWithOptions(
+                              signed: true, decimal: true),
+                          inputBorder: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          onSaved: (PhoneNumber number) {},
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
                       TextFormField(
                           controller: _emailController,
                           keyboardType: TextInputType.emailAddress,
@@ -101,27 +158,13 @@ class _RegisterPageState extends State<RegisterPage> {
                               floatingLabelBehavior:
                                   FloatingLabelBehavior.never),
                           validator: (String? text) {
-                            final String? valid =
-                                AuthUtils.textFormFieldValidator(text, 'Email');
-                            return valid;
-                          }),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                          controller: _phoneController,
-                          keyboardType: TextInputType.phone,
-                          decoration: const InputDecoration(
-                              labelText: 'Phone',
-                              labelStyle: kLabelStyleAuthPages,
-                              border: kBoderTextFormField,
-                              focusedBorder: kFocusedBorderTextFormField,
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never),
-                          validator: (String? text) {
-                            final String? valid =
-                                AuthUtils.textFormFieldValidator(text, 'Phone');
-                            return valid;
+                            String email = _emailController.text;
+                            final bool isValid = EmailValidator.validate(email);
+                            if (isValid) {
+                              return null;
+                            } else {
+                              return 'The email is not valid';
+                            }
                           }),
                       const SizedBox(
                         height: 10,
@@ -158,8 +201,8 @@ class _RegisterPageState extends State<RegisterPage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Checkbox(
-                           activeColor: Colors.green,
-                            checkColor: Colors.white,
+                              activeColor: Colors.green,
+                              checkColor: Colors.white,
                               value: _isAccepted,
                               onChanged: (bool? accepted) {
                                 setState(() {
@@ -188,10 +231,7 @@ class _RegisterPageState extends State<RegisterPage> {
                           ),
                           GestureDetector(
                             onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const LoginPage()));
+                              Navigator.pop(context);
                             },
                             child: const Text(
                               'Sign In',
@@ -208,7 +248,7 @@ class _RegisterPageState extends State<RegisterPage> {
                   AuthButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-                        //register();
+                        _registerUser();
                       }
                     },
                     title: 'Sign Up',
@@ -220,5 +260,25 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
       ),
     );
+  }
+
+  void _registerUser() async {
+    await checkRegister.controlRegister(_userNameController.text,
+        _phoneController.text, _emailController.text, _passwordController.text);
+
+    if (mounted) {
+      if (checkRegister.getSucessRegister) {
+        final User user = checkRegister.getUser!;
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => HomePage(
+                      userLogged: user,
+                    )));
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(buildAuthSnackBar(checkRegister.getError));
+      }
+    }
   }
 }
