e65ec92e56c5935d53b21a8a77aaaed9afeb0490
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-08 11:57:02 -0300

    feat: endpoint addUser

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -1,5 +1,6 @@
-class Endpoints{
-  static const String urlApi = 'dummyjson.com'; 
+class Endpoints {
+  static const String urlApi = 'dummyjson.com';
   static const String authLogin = 'auth/login';
   static const String authMe = 'auth/me';
+  static const String addUser = '/users/add';
 }
