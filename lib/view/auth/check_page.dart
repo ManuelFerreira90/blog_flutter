1ca514a6f0a7111e81934d62f03035ad1e5ade23
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-10 09:37:29 -0300

    refactor: verificação página montada

--- lib/view/auth/check_page.dart
+++ lib/view/auth/check_page.dart
@@ -1,5 +1,6 @@
 import 'package:blog_mobile/controllers/auth/check_controller.dart';
 import 'package:blog_mobile/models/user.dart';
+import 'package:blog_mobile/themes/style/theme_colors.dart';
 import 'package:blog_mobile/view/auth/login_page.dart';
 import 'package:blog_mobile/view/home/home_page.dart';
 import 'package:flutter/material.dart';
@@ -24,14 +25,18 @@ class _CheckPageState extends State<CheckPage> {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: CircularProgressIndicator(),
+      child: CircularProgressIndicator(
+        color: ThemeColors.colorCircularProgressIndicator,
+      ),
     );
   }
 
   void _checkUser() async {
     final CheckController checkUserControl = CheckController();
+    await checkUserControl.controlCheckUser();
 
-    if (checkUserControl.user != null) {
+    if(mounted){
+      if (checkUserControl.isLogged) {
       final User user = checkUserControl.user!;
       Navigator.push(
           context,
@@ -42,6 +47,7 @@ class _CheckPageState extends State<CheckPage> {
     } else {
       Navigator.push(
           context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    }
     }
   }
 }
