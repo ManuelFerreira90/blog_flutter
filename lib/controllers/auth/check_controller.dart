24b72130dca18536dcea4fa9405e4c283dbd6759
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-10 09:36:20 -0300

    refactor: nova lógica

--- lib/controllers/auth/check_controller.dart
+++ lib/controllers/auth/check_controller.dart
@@ -1,25 +1,35 @@
+import 'dart:async';
+
 import 'package:blog_mobile/api/auth/auth_service.dart';
 import 'package:blog_mobile/models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CheckController{
+class CheckController {
   bool _isLogged = false;
   User? _user;
 
-  CheckController(){
-    _checkUser();
-  }
+  CheckController();
 
-  Future<void> _checkUser() async{
-    _user = await AuthService.currentAuthUser();
+  Future<void> _checkUser(String token) async {
+    _user = await AuthService.currentAuthUser(token);
     _isLogged = _user != null;
   }
 
-  bool get isLogged{
+  Future<void> controlCheckUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    if (token != null) {
+      await _checkUser(token);
+    } else {
+      _user = null;
+    }
+  }
+
+  bool get isLogged {
     return _isLogged;
   }
 
-  User? get user{
+  User? get user {
     return _user;
   }
-
 }
