a8f5f55b30fbb22cff32ecd60fe38dff2098f6ed
ManuelFerreira90 <ribeirobr009@gmail.com>
2024-06-10 09:29:17 -0300

    feat: endpoints posts

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -3,4 +3,7 @@ class Endpoints {
   static const String authLogin = 'auth/login';
   static const String authMe = 'auth/me';
   static const String addUser = '/users/add';
+  static const String posts = '/posts';
+  static const String getUser = '/users';
+  static const String getPostsUser = '/posts/user';
 }
